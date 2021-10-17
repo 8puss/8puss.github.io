dbf5bc84ebcfc18d0dc953998b58f7c463d06c13
8pus <rodrx20@gmail.com>
2021-10-17 18:52:20 -0500

    adding graphic to production

---
+++ src/production-en.js
@@ -0,0 +1,6 @@
+const graphImage = document.getElementById('graph-img1');
+
+graphImage.addEventListener('click', function() {
+    const description = document.getElementById('img-description-en');
+    description.innerText = "(Cover) P.R.E.G.A. GoldGang";
+});
---
+++ src/production.js
@@ -0,0 +1,6 @@
+const graphImage = document.getElementById('graph-img1');
+
+graphImage.addEventListener('click', function() {
+    const description = document.getElementById('img-description');
+    description.innerText = "Portada P.R.E.G.A. GoldGang";
+});
