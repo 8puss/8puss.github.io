d9849d5c0e0372e0eba118bb9f4dd3e1d4d8144f
rdrx <rodrx20@gmail.com>
2022-01-17 18:24:00 -0600

    adding some projects to portfolio and a couple of styles on hover navbar and shadow on pic

--- src/index-en.js
+++ src/index-en.js
@@ -5,10 +5,10 @@ const backTitle = document.getElementById('back-end-title');
 
 frontTitle.addEventListener('click', function() {
     const frontText = document.getElementById('answer-text');
-    frontText.innerText = "HTML5 layout and CSS responsive design. DOM handling and performance through Javascript (ES6+).";
+    frontText.innerText = "HTML5 layout and CSS responsive design. DOM handling and performance through Javascript (ES6+) and Vue.js 🤓.";
 });
 
 backTitle.addEventListener('click', function() {
     const backText = document.getElementById('answer-text');
-    backText.innerText = "API REST development using Node.js and Express.js. Deploy with Heroku";
+    backText.innerText = "API REST development using Node.js and Express.js. Deploy with Heroku/Netlify. PostgreSQL as database and Sequelize as ORM. Passport.js and JWT as authentication and authorización protocols. 💻";
 });
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ const backTitle = document.getElementById('back-end-title');
 
 frontTitle.addEventListener('click', function() {
     const frontText = document.getElementById('answer-text');
-    frontText.innerText = "Maquetación con HTML5 y responsive design con CSS3. Manipulación del DOM y funcionamiento con Javascript (ES6+).";
+    frontText.innerText = "Maquetación con HTML5 y responsive design con CSS3. Manipulación del DOM y funcionamiento con Javascript (ES6+) y Vue.js 🤓";
 });
 
 backTitle.addEventListener('click', function() {
     const backText = document.getElementById('answer-text');
-    backText.innerText = "Desarrollo de una API REST con Node.js y Express.js. Deploy en Heroku";
+    backText.innerText = "Desarrollo de una API REST con Node.js y Express.js. Deploy en Heroku/Netlify. Base de datos con PostgreSQL y Sequelize como ORM. Passport.js y JWT como protocolos de autenticación y autorización. 💻";
 });
