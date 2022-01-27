074e316709dae653e488a61a0847a7e5e862861e
rdrx <rodrx20@gmail.com>
2022-01-26 20:16:28 -0600

    adding webpack y react en eventlistener portfolio

--- src/index-en.js
+++ src/index-en.js
@@ -1,14 +1,16 @@
 // DOM
 
-const frontTitle = document.getElementById('front-end-title');
-const backTitle = document.getElementById('back-end-title');
+const frontTitle = document.getElementById("front-end-title");
+const backTitle = document.getElementById("back-end-title");
 
-frontTitle.addEventListener('click', function() {
-    const frontText = document.getElementById('answer-text');
-    frontText.innerText = "HTML5 layout and CSS responsive design. DOM handling and performance through Javascript (ES6+) and Vue.js 🤓.";
+frontTitle.addEventListener("click", function () {
+  const frontText = document.getElementById("answer-text");
+  frontText.innerText =
+    "HTML5 layout and CSS responsive design. DOM handling and performance through Javascript (ES6+) and its frameworks just as Vue.js or React.js and Webpack as module bundler. 🤓.";
 });
 
-backTitle.addEventListener('click', function() {
-    const backText = document.getElementById('answer-text');
-    backText.innerText = "API REST development using Node.js and Express.js. Deploy with Heroku/Netlify. PostgreSQL as database and Sequelize as ORM. Passport.js and JWT as authentication and authorización protocols. 💻";
+backTitle.addEventListener("click", function () {
+  const backText = document.getElementById("answer-text");
+  backText.innerText =
+    "API REST development using Node.js and Express.js. Deploy with Heroku/Netlify. PostgreSQL as database and Sequelize as ORM. Passport.js and JWT as authentication and authorización protocols. 💻";
 });
